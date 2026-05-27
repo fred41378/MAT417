57bc910592ce3ec8f246631e8c5c0769778abf5a
Frederick <Frederick.Taylor@usherbrooke.ca>
2026-05-27 18:08:17 -0400

    Dev1 - Ajout des sous-questions 1-3 de la partie code

--- devoir_1/question_6.py
+++ devoir_1/question_6.py
@@ -1,10 +1,112 @@
 import matplotlib.pyplot as plt
-import random
+import numpy as np
+import time
 
-n = 10
+n0 = 10000
+n1 = 500000
+n = np.arange(start=n0, stop= n1 + 10000, step=10000)
 
-x = [random.random() for i in range(n)]
+err2 = []
+err3 = []
+err4 = []
+err5 = []
 
-print(x)
-plt.plot(x)
+times2 = []
+times3 = []
+times4 = []
+times5 = []
+
+
+for i in n:
+    x = np.random.default_rng().random(i, dtype=np.float32)
+    methode1 = sum(np.float64(x))
+
+    start_time = time.perf_counter()
+    methode2 = sum(np.float32(x))
+    times2.append(max(time.perf_counter() - start_time, 1e-12))
+
+    s = x[0]
+    c = 0
+    start_time = time.perf_counter()
+    for j in range(1, i):
+        y = x[j] - c
+        t = s + y
+        c = (t - s) - y
+        s = t
+    methode3 = np.float32(s)
+    times3.append(max(time.perf_counter() - start_time, 1e-12))
+
+    start_time = time.perf_counter()
+    x_sorted_asc = np.float32(np.sort(x))
+    methode4 = sum(x_sorted_asc)
+    times4.append(max(time.perf_counter() - start_time, 1e-12))
+
+    start_time = time.perf_counter()
+    x_sorted_desc = np.float32(np.sort(x[::-1]))
+    methode5 = sum(x_sorted_desc)
+    times5.append(max(time.perf_counter() - start_time, 1e-12))
+
+    err2.append(abs(methode2 - methode1))
+    err3.append(abs(methode3 - methode1))
+    err4.append(abs(methode4 - methode1))
+    err5.append(abs(methode5 - methode1))
+
+
+
+plt.plot(n, err2, color='orange', label='err2', linestyle='dashdot')
+plt.plot(n, err3, color='green', label='err3', linestyle='solid')
+plt.plot(n, err4, color='red', label='err4', linestyle='solid')
+plt.plot(n, err5, color='blue', label='err5', linestyle='dotted')
+plt.legend()
+plt.title('Devoir 1')
+plt.xlabel('n')
+plt.ylabel('Error')
+plt.show()
+
+meanerr2 = np.mean(err2)
+meanerr3 = np.mean(err3)
+meanerr4 = np.mean(err4)
+meanerr5 = np.mean(err5)
+
+print("Classement des methodes : \n")
+print("4e: methode4 (" + str(meanerr4) +")")
+print("4e: methode5 (" + str(meanerr5) + ")")
+print("2e: methode2 (" + str(meanerr2) + ")")
+print("1e: methode3 (" + str(meanerr3) + ")")
+print("On peut voir que les méthodes qui orde en ordre croissant ou décroissant sont égales \n ",
+      "et que la meilleur méthode est de loins la 3e")
+
+log_n = np.log(n)
+log_times2 = np.log(times2)
+log_times3 = np.log(times3)
+log_times4 = np.log(times4)
+log_times5 = np.log(times5)
+
+k2, a2 = np.polyfit(log_n, log_times2, 1)
+k3, a3 = np.polyfit(log_n, log_times3, 1)
+k4, a4 = np.polyfit(log_n, log_times4, 1)
+k5, a5 = np.polyfit(log_n, log_times5, 1)
+
+reg2 = a2 + k2 * log_n
+reg3 = a3 + k3 * log_n
+reg4 = a4 + k4 * log_n
+reg5 = a5 + k5 * log_n
+
+plt.figure()
+
+plt.plot(log_n, log_times2, 'o', label='methode2')
+plt.plot(log_n, log_times3, 'o', label='methode3')
+plt.plot(log_n, log_times4, 'o', label='methode4')
+plt.plot(log_n, log_times5, 'o', label='methode5')
+
+plt.plot(log_n, reg2, label='reg2')
+plt.plot(log_n, reg3, label='reg3')
+plt.plot(log_n, reg4, label='reg4')
+plt.plot(log_n, reg5, label='reg5')
+
+plt.xlabel("ln(n)")
+plt.ylabel("ln(T(n))")
+plt.title("Régression log-log")
+plt.legend()
+
 plt.show()
